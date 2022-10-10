5add74dd253415f939592d58c903861b314f1ff4
mcli28 <mclimber28@gmail.com>
2022-10-09 21:41:29 -0400

    -let -template literal -arrow function

--- React The Complete Guide With React Hook Redux in 4hr/firstapp/src/App.js
+++ React The Complete Guide With React Hook Redux in 4hr/firstapp/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  /*const user = {}
+  user.name = "limber"
+  console.log(user);
+
+  const arr = ["orange"]
+  arr.push("apple")
+  console.log(arr); 
+
+  function fruitColor(fruit) {
+    switch (fruit) {
+      case "apple":{
+        let color = "red"
+        return color
+      }
+      case "banana":{
+          let color = "yellow"
+        return color
+      }
+
+      default:
+        return "other colors";
+    }
+  }
+
+  console.log(fruitColor("apple"));
+  console.log(fruitColor("banana"));
+  console.log(fruitColor("orange"));
+  console.log(fruitColor("berry"));
+  console.log("something")
+  const name = "mike"
+  console.log('I\'m '+name+'. Nice to meet you');
+  console.log(`I'm ${name}. Nice to meet you`);
+
+  function sayMyName(name) {
+    return `my name is ${name}`
+  }
+  console.log(sayMyName("limber"))
+
+  const sayMyName2 = (name) => {
+    return `my name is ${name}`
+  }
+
+  const sayMyName3 = name => `my name is ${name}`
+
+  console.log(sayMyName("limber"))
+  console.log(sayMyName2("limber mc"))
+  console.log(sayMyName3("limber mcx"))*/
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello this is my first app</h1>
+
     </div>
   );
 }
