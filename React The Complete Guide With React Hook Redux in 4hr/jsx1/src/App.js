c8493a495484fee21da3465277a3512d94fe1385
mcli28 <mclimber28@gmail.com>
2022-10-25 21:31:46 -0400

    what is JSX, import CSS'

---
+++ React The Complete Guide With React Hook Redux in 4hr/jsx1/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Custom.css'
+
+function App() {
+
+  //Background-color -> backgroundColor, border-color -> borderColor
+  //Value need to add single quote
+  //Semi colon --> comma
+  return (
+    <div className="outerdiv">
+      <div className="innerdiv">
+        This is a green box
+      </div>
+    </div>
+  );
+}
+
+export default App;
