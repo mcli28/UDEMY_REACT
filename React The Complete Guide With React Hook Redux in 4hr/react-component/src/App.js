f51155ac57e2ebdae246d149eb0324b231bd5caa
mcli28 <mclimber28@gmail.com>
2022-10-27 09:58:43 -0400

    -create list card -create card component

---
+++ React The Complete Guide With React Hook Redux in 4hr/react-component/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Card from './Card';
+
+function App() {
+  return (
+    <div className="App">
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+    </div>
+  );
+}
+
+export default App;
