6907442351739a3f2697b7dfccf3052e0230ed1b
mcli28 <mclimber28@gmail.com>
2022-10-29 09:25:22 -0400

    *pass and receive props *pass multiple props *The children of props

--- React The Complete Guide With React Hook Redux in 4hr/react-component/src/App.js
+++ React The Complete Guide With React Hook Redux in 4hr/react-component/src/App.js
@@ -1,13 +1,37 @@
 import React from 'react'
 import './App.css'
 import Card from './Card';
+import { faker } from '@faker-js/faker';
 
 function App() {
+
+  const buttonsMarkup = (
+    <div>
+      <button className='button button2'>YES</button>
+      <button className='button button3'>NO</button>
+    </div>
+  )
+
   return (
     <div className="App">
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
+      <Card
+        avatar={faker.image.avatar()} 
+        name={`${faker.name.firstName()} ${faker.name.firstName()}`} 
+        title={faker.name.jobTitle()}>
+          {buttonsMarkup}
+      </Card>
+      <Card
+        avatar={faker.image.avatar()} 
+        name={`${faker.name.firstName()} ${faker.name.firstName()}`} 
+        title={faker.name.jobTitle()}>
+          {buttonsMarkup}
+      </Card>
+      <Card
+        avatar={faker.image.avatar()} 
+        name={`${faker.name.firstName()} ${faker.name.firstName()}`} 
+        title={faker.name.jobTitle()}>
+          {buttonsMarkup}
+      </Card>
     </div>
   );
 }
--- React The Complete Guide With React Hook Redux in 4hr/react-component/src/Card.js
+++ React The Complete Guide With React Hook Redux in 4hr/react-component/src/Card.js
@@ -2,13 +2,15 @@ import React from 'react'
 import './App.css';
 
 
-const Card = () => {
+const Card = props => {
+  console.log(props)
   return (
     <div className="card">
-        <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style={{width: '100%'}}/>
+        <img src={props.avatar} alt="Avatar" style={{width: '100%'}}/>
         <div className="container">
-            <h4><b>John Doe</b></h4> 
-            <p>Architect & Engineer</p> 
+            <h4><b>{props.name}</b></h4> 
+            <p>{props.title}</p> 
+            <p>{props.children}</p> 
         </div>
     </div>
   )
