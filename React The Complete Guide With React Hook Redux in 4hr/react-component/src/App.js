1aa991889a91db991f837cd9710e46fa2d434f11
mcli28 <mclimber28@gmail.com>
2022-11-06 21:09:33 -0400

    list -rendering a list of cards -delete a specific card -fix unique key error -change the name of a specific card

--- React The Complete Guide With React Hook Redux in 4hr/react-component/src/App.js
+++ React The Complete Guide With React Hook Redux in 4hr/react-component/src/App.js
@@ -4,28 +4,57 @@ import Card from './Card';
 import { faker } from '@faker-js/faker';
 
 function App() {
-  const [name, setName] = useState('Alan Smith')
+  const [cards, setCards] = useState([
+    {
+      id: '12',
+      name: 'David Schuster',
+      title: 'International Operations Producer',
+      avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/765.jpg'
+    },
+    {
+      id: '123',
+      name: 'Bart Dietrich',
+      title: 'Future security developer',
+      avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/764.jpg'
+    },
+    {
+      id: '1234',
+      name: 'Javier Milei',
+      title: 'Forward metrics analitics',
+      avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/763.jpg'
+    }
+  ])
   const [showCard, setShowCard] = useState(true)
-
-  const changeNameHandler = name => setName(name)
-  const changeInputHandler = event => setName(event.target.value)
   const toggleShowCard = () => setShowCard(!showCard)
+  const deleteCardHandler = (cardIndex) => {
+    const cards_copy = [...cards]
+    cards_copy.splice(cardIndex, 1)
+    console.log('cards_copy', cards_copy);
+    console.log('cards', cards);
+    setCards(cards_copy)
+  }
 
-  const buttonsMarkup = (
-    <div>Investor Integration Consultant
-      <button className='button button2'>YES</button>
-      <button className='button button3'>NO</button>
-    </div>
-  )
-  const cardsMarkup = showCard && <Card
-    avatar='https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/531.jpg' 
-    name={name} 
-    title='Investor Integration Consultant'
-    onChangeName={()=>changeNameHandler('Michael Chan')}
-    onChangeInput={changeInputHandler}
-    >
-      {buttonsMarkup}
-  </Card>
+  const changeNameHandler = (event,id) => {
+    //1. which card
+    const cardIndex = cards.findIndex(card=>card.id == id)
+    //2. make a copy of the cards
+    const cards_copy = [...cards]
+    //3. change the name of the specific card
+    cards_copy[cardIndex].name = event.target.value
+    //4. set the cards with the lastest version of card copy
+    setCards(cards_copy)
+  }
+
+  const cardsMarkup = showCard && (
+      cards.map((card, index) => <Card
+        avatar={card.avatar} 
+        name={card.name} 
+        title={card.title}
+        key={card.id}
+        onDelete={()=>deleteCardHandler(index)}
+        onChangeName = {(event) => changeNameHandler(event, card.id)}
+      />)
+    )
   
   return (
     <div className="App">
--- React The Complete Guide With React Hook Redux in 4hr/react-component/src/Card.js
+++ React The Complete Guide With React Hook Redux in 4hr/react-component/src/Card.js
@@ -3,16 +3,15 @@ import './App.css';
 
 
 const Card = props => {
-  console.log(props)
   return (
     <div className="card">
         <img src={props.avatar} alt="Avatar" style={{width: '100%'}}/>
         <div className="container">
             <h4><b>{props.name}</b></h4> 
             <p>{props.title}</p>
-            <input type="text" onChange={props.onChangeInput} value={props.name}/>
-            <p><button className='button' onClick={props.onChangeName}>Change Name</button></p>
-            <div>{props.children}</div> 
+            <input type="text" onChange={props.onChangeName} valuer={props.name}/>
+            <p><button className='button button-red' onClick={props.onDelete}>Delete</button></p>
+            <div>{props.children}</div>
         </div>
     </div>
   )
