7392bff5274e48f89a0d0d5b3f0165171d7295ad
mcli28 <mclimber28@gmail.com>
2022-11-12 16:21:59 -0400

    React hooks to improve the performance of the app - useEffect fetch data with axios - useEffect fetch a card

--- React The Complete Guide With React Hook Redux in 4hr/react-component/src/App.js
+++ React The Complete Guide With React Hook Redux in 4hr/react-component/src/App.js
@@ -1,9 +1,10 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css'
 import Card from './Card';
 import { ThemeProvider } from 'styled-components'
-import Button from './element/Button'
+//import Button from './element/Button'
 //import { faker } from '@faker-js/faker';
+import axios from 'axios'
 
 const theme = {
   primary: '#4CAF50',
@@ -11,71 +12,36 @@ const theme = {
 }
 
 function App() {
-  const [cards, setCards] = useState([
-    {
-      id: '12',
-      name: 'David Schuster',
-      title: 'International Operations Producer',
-      avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/765.jpg'
-    },
-    {
-      id: '123',
-      name: 'Bart Dietrich',
-      title: 'Future security developer',
-      avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/764.jpg'
-    },
-    {
-      id: '1234',
-      name: 'Javier Milei',
-      title: 'Forward metrics analitics',
-      avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/763.jpg'
-    }
-  ])
-  const [showCard, setShowCard] = useState(true)
-  const toggleShowCard = () => setShowCard(!showCard)
-  const deleteCardHandler = (cardIndex) => {
-    const cards_copy = [...cards]
-    cards_copy.splice(cardIndex, 1)
-    console.log('cards_copy', cards_copy);
-    console.log('cards', cards);
-    setCards(cards_copy)
-  }
-
+  const [card, setCard] = useState([])
+  const [id, setId] = useState(1)
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then(res=>{
+        console.log(res.data)
+        setCard(res.data)
+      })
+  },[id])
+  
   const changeNameHandler = (event,id) => {
-    //1. which card
-    const cardIndex = cards.findIndex(card=>card.id === id)
-    //2. make a copy of the cards
-    const cards_copy = [...cards]
-    //3. change the name of the specific card
-    cards_copy[cardIndex].name = event.target.value
-    //4. set the cards with the lastest version of card copy
-    setCards(cards_copy)
+    //1. make a copy of the cards
+    const card_copy = {...card}
+    //2. change the name of the specific card
+    card_copy.name = event.target.value
+    //3. set the cards with the lastest version of card copy
+    setCard(card_copy)
   }
 
-  //const buttonStyle = {
-  //  backgroundColor: null
-  //}
-  const classes = ['button']
-  if (cards.length<3) classes.push('pink')
-  if (cards.length<2) classes.push('red')
-
-  const cardsMarkup = showCard && (
-      cards.map((card, index) => <Card
-        avatar={card.avatar} 
-        name={card.name} 
-        title={card.title}
-        key={card.id}
-        onDelete={()=>deleteCardHandler(index)}
-        onChangeName = {(event) => changeNameHandler(event, card.id)}
-      />)
-    )
   
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <Button color="mango" length={cards.length}>Toggle</Button>
-        <button className={classes.join(' ')} onClick={toggleShowCard}>Toggle show/hide</button>
-        {cardsMarkup}
+        <input type="text" value={id} onChange={e=>setId(e.target.value)}/>
+        <Card
+          name={card.name} 
+          phone={card.phone}
+          key={card.id}
+          onChangeName = {(event) => changeNameHandler(event, card.id)}
+        />
       </div>
     </ThemeProvider>
   );
