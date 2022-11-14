f3700b48ea8c1273045dc14514455ba00269993f
mcli28 <mclimber28@gmail.com>
2022-11-14 07:45:39 -0400

    react hooks-to improve the performance of the app *useEffect fetch data with axios*useEffect fetch a card*react Context*useContext

---
+++ React The Complete Guide With React Hook Redux in 4hr/teach-use-context/src/components/ComponentC.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { NameContext } from '../App'
+import { ColorContext } from '../App'
+const ComponentC = () => {
+  return (
+    <div>
+        <div>ComponentC</div>
+        <NameContext.Consumer>
+            {name=>{
+                return (
+                    <ColorContext.Consumer>
+                        {color => (
+                            <div>name: {name}, color {color}</div>
+                        )}
+                    </ColorContext.Consumer>
+                )
+            }}
+        </NameContext.Consumer>
+    </div>
+  )
+}
+
+export default ComponentC
