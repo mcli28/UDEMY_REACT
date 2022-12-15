75c8bda0d30bd99e001ff0a9b536214a86b1f108
mcli28 <mclimber28@gmail.com>
2022-12-14 22:33:50 -0400

    -fetch the posts by using axios

--- React The Complete Guide With React Hook Redux in 4hr/teach-router/src/components/Home.js
+++ React The Complete Guide With React Hook Redux in 4hr/teach-router/src/components/Home.js
@@ -1,10 +1,30 @@
-import React, {useEffect} from 'react'
-import {faker} from '@faker-js/faker'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+//import {faker} from '@faker-js/faker'
 const Home = () => {
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    (async()=>{
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+      setPosts(res.data.slice(0, 5))
+    })()
+  }, [])
+
+  const listMarkup = posts.length > 0 ? (
+    posts.map(post => (
+      <div className ="card shadow p-3 mb-5 bg-white " key={post.id}>
+        <div className ="card-body">
+          <h5 className ="card-title">{post.title}</h5>
+          <p className ="card-text">{post.body}</p>
+        </div>
+      </div>
+    ))
+  ) : (<div className='text-center'>No posts found</div>)
+
   return (
     <div className='container'>
         <h4 className='center'>Home</h4>
-        <p>{`${faker.lorem.paragraph()}`}</p>
+        {listMarkup}
     </div>
   )
 }
