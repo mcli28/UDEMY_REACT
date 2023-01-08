188137aa76d36f1e2feaff3b6848871047273e89
mcli28 <mclimber28@gmail.com>
2023-01-07 20:36:01 -0400

    -make suggestions base on user input -render the suggestions list -select the suggestions

--- React The Complete Guide With React Hook Redux in 4hr/react-autocomplete/src/App.js
+++ React The Complete Guide With React Hook Redux in 4hr/react-autocomplete/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import './custom.css'
 import axios from 'axios'
 import {useEffect, useState} from 'react'
 
 function App() {
   const [users, setUsers] = useState([])
   const [text, setText] = useState('')
-
+  const [suggestions, setSuggestions] = useState([])
   useEffect(() => {
     const loadUsers = async()=>{
       const response = await axios.get('https://reqres.in/api/users')
@@ -14,8 +15,21 @@ function App() {
     }
     loadUsers()
   }, [])
+  const onSuggestHandler = (text) => {
+    setText(text)
+    setSuggestions([])
+  }
   const onChangeHandler = (text) => {
-   setText(text)
+    let matches = []
+    if (text.length > 0){
+      matches = users.filter(user => {
+        const regex = new RegExp(`${text}`,"gi")
+        return user.email.match(regex)
+      })
+    }
+    console.log('matches', matches);
+    setSuggestions(matches)
+    setText(text)
   }
   return (
     <div className="container">
@@ -23,7 +37,16 @@ function App() {
       <input type="text" className='col-md-12 input' style={{marginTop:10}}
         onChange={e=>onChangeHandler(e.target.value)}
         value={text}
+        onBlur={() => {
+          setTimeout(() => {
+            setSuggestions([])
+          }, 100)
+        }}
       />
+      {suggestions && suggestions.map((suggestion, i) =>
+        <div key={i} className="suggestion col-md-12 justify-content-md-center"
+          onClick={() => onSuggestHandler(suggestion.email)}>{suggestion.email}</div>
+      )}
     </div>
   );
 }
